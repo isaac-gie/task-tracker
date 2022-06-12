a1d8bf7a0e9da3236fa7adea7634b1e4fffce795
Izzy <isaacggitonga@gmail.com>
2022-06-12 21:37:38 +0300

    Added some changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,20 @@
+import Proptypes from 'prop-types'
 import {FaTimes} from 'react-icons/fa'
 
 const Task = ({ task }) => {
   return (
     <div className="task">
         <h3>
-            {task.text} <FaTimes />
+            {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} />
         </h3>
         <p>{task.day}</p>
     </div>
   )
+}
+
+FaTimes.propTypes = {
+    color: Proptypes.string,
+    cursor: Proptypes.string,
 }
 
 export default Task
