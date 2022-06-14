47d4086d9aa100f65c33dca965865620c6180894
Izzy <isaacggitonga@gmail.com>
2022-06-14 13:29:31 +0300

    Added the AddTask component and made some more changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
-  const[tasks, setTasks] = useState ([
+  const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctors Appointement',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
+      id: 1,
+      text: "Doctors Appointement",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Food Shoping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
+      id: 3,
+      text: "Food Shoping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
     },
-])
+  ]);
 
-// Delete task
-const deleteTask = (id) => {
-  console.log('delete', id)
-}
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toogle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+
+      <AddTask />
+
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "YOU HAVE NO PENDING TASKS"
+      )}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,29 @@
-import Proptypes from 'prop-types'
-import {FaTimes} from 'react-icons/fa'
+import Proptypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
-        <h3>
-            {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} />
-        </h3>
-        <p>{task.day}</p>
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+
+      <p>{task.day}</p>
     </div>
-  )
-}
+  );
+};
 
 FaTimes.propTypes = {
-    color: Proptypes.string,
-    cursor: Proptypes.string,
-}
+  color: Proptypes.string,
+  cursor: Proptypes.string,
+};
 
-export default Task
+export default Task;
